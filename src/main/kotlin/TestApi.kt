610e808af02fcf2688fc1e1439d7828144a1c7f7
Tõnis Tiganik <ttiganik@high-mobility.com>
2019-11-21 09:59:53 +0700

    initial commit

---
+++ src/main/kotlin/TestApi.kt
@@ -0,0 +1,12 @@
+class TestApi {
+    companion object {
+        /**
+         * A test function.
+         *
+         * @param param The param
+         */
+        fun testFun(param: String) {
+            println(String.format("testFun %s", param))
+        }
+    }
+}
